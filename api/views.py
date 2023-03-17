d99f90b606a6ae45c8e8d29d2cfd1e2a5096bab7
Temidayo-icon <65119571+Temidayo-icon@users.noreply.github.com>
2023-03-17 14:44:04 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -5,13 +5,8 @@ from rest_framework.response import Response
 from .models import Reading
 from .serializers import ReadingSerializer
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def create_reading(request):
-    if request.method == 'GET':
-        readings = Reading.objects.all()
-        serializer = ReadingSerializer(readings, many=True)
-        return Response(serializer.data, status=200)
-    elif request.method == 'POST':
     serializer = ReadingSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
