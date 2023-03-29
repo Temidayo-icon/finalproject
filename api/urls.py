a828657b5f7c8c728878cb0af33984bf58c5d820
Temidayo-icon <65119571+Temidayo-icon@users.noreply.github.com>
2023-03-29 04:10:25 +0100

    reset data url update

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import create_reading
 from .views import get_readings
+from .views import reset_data
+
 
 urlpatterns = [
     path('api/post', create_reading, name='create_reading'),
      path('api/get', get_readings, name='get_readings'),
+        path('api/reset', reset_data, name='reset_readings'),
+
 ]
