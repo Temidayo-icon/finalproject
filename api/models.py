1e019879bdf3afc44897d255c06a99949a3e0179
Temidayo-icon <65119571+Temidayo-icon@users.noreply.github.com>
2023-05-20 16:33:27 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ class Reading(models.Model):
     voltage = models.FloatField()
     current = models.FloatField()
     power = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.power
